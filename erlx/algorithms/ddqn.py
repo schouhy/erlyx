46c86a5fd4fa91e708ad3a7029afe70d73a3f885
Sergio Chouhy <sergio.chouhy@gmail.com>
2020-07-19 19:38:14 -0300

    minor refactor

--- erlx/algorithms/ddqn.py
+++ erlx/algorithms/ddqn.py
@@ -26,10 +26,9 @@ class DoubleDeepQLearner(BaseLearner):
         next_states = torch.stack(list(map(torch.Tensor, next_states)))
 
         # compute target
-        next_q_values = self.policy.model(next_states)
-        next_q_action = next_q_values.argmax(1)
+        next_action = self.policy.model(next_states).argmax(1)
         next_q_values_target = self.target_policy.model(next_states)
-        next_q_value = next_q_values_target.gather(1, torch.LongTensor(next_q_action).view(-1, 1)).view(-1)
+        next_q_value = next_q_values_target.gather(1, torch.LongTensor(next_action).view(-1, 1)).view(-1)
         expected_q_value = rewards + 0.99 * next_q_value * (1. - torch.FloatTensor(dones))
         expected_q_value = expected_q_value.detach().view(-1)
 
