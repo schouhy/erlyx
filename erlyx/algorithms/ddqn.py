2f989478e1c117e3a18baa21f44dc3f0a6a9d57e
Sergio Chouhy <sergio.chouhy@gmail.com>
2020-08-01 16:51:36 -0300

    Use PytorchPolicy protocol. Added parameters to init

--- erlyx/algorithms/ddqn.py
+++ erlyx/algorithms/ddqn.py
@@ -1,50 +1,71 @@
 from erlyx.learners import BaseLearner
-
+from erlyx.policies import PytorchPolicy
+from typing import Callable
 import torch
 import copy
 
 
 class DoubleDeepQLearner(BaseLearner):
-    def __init__(self, batch_size, policy, learning_rate, sync_frequency):
+    def __init__(
+            self,
+            batch_size: int,
+            policy: PytorchPolicy,
+            learning_rate: float,
+            sync_frequency: int,
+            gamma: float = 0.99,
+            loss_func: Callable = torch.nn.MSELoss()
+    ):
         self.policy = policy
-        self.target_policy = copy.deepcopy(policy)
-        self.target_policy.model.eval()
         self.batch_size = batch_size
         self.optimizer = torch.optim.Adam(policy.model.parameters(), lr=learning_rate)
         self.sync_frequency = sync_frequency
         self.losses = []
         self.update_counter = 0
+        self.gamma = gamma
+        self.loss_func = loss_func
+        self.target_policy = self._make_target_policy()
+
+    def _make_target_policy(self):
+        target_policy = copy.deepcopy(self.policy)
+        for parameter in target_policy.model.parameters():
+            parameter.requires_grad = False
+        target_policy.model.eval()
+        return target_policy
 
     def update(self, dataset):
         self.update_counter += 1
         self.policy.model.train()
-        # build minibatch
+        # Fetch minibatch
         mini_batch = dataset.sample(self.batch_size)
         states, actions, next_states, rewards, dones = zip(*mini_batch)
-        x_batch = torch.stack(list(map(torch.Tensor, states)))
+
+        # Convert types
+        states = torch.stack(list(map(self.policy.process_observation, states)))
+        next_states = torch.stack(list(map(self.target_policy.process_observation, next_states)))
         rewards = torch.stack(list(map(torch.tensor, rewards)))
-        next_states = torch.stack(list(map(torch.Tensor, next_states)))
+        actions = torch.LongTensor(actions)
+        dones = torch.FloatTensor(dones)
+
+        # Compute Q values
+        q_values = self.policy.model(states)
+        q_value = q_values.gather(1, actions.view(-1, 1)).view(-1)
 
         # compute target
         next_action = self.policy.model(next_states).argmax(1)
         next_q_values_target = self.target_policy.model(next_states)
-        next_q_value = next_q_values_target.gather(1, torch.LongTensor(next_action).view(-1, 1)).view(-1)
-        expected_q_value = rewards + 0.99 * next_q_value * (1. - torch.FloatTensor(dones))
-        expected_q_value = expected_q_value.detach().view(-1)
+        next_q_value = next_q_values_target.gather(1, next_action.view(-1, 1)).view(-1)
+        target = rewards + self.gamma * next_q_value * (1. - dones)
 
-        # compute ind variable
-        q_values = self.policy.model(x_batch)
-        q_value = q_values.gather(1, torch.LongTensor(actions).view(-1, 1)).view(-1)
-
-        loss = torch.nn.MSELoss()(q_value, expected_q_value)
-        # loss = torch.nn.SmoothL1Loss()(q_value, expected_q_value)
+        # Loss
+        loss = self.loss_func(q_value, target)
         self.losses.append(loss.data.cpu().numpy())
 
+        # Gradient descent
         self.optimizer.zero_grad()
         loss.backward()
         # torch.nn.utils.clip_grad_norm_(policy.model.parameters(), 0.5)
         self.optimizer.step()
 
+        # Update target policy
         if self.update_counter % self.sync_frequency == 0:
-            self.target_policy = copy.deepcopy(self.policy)
-            self.target_policy.model.eval()
+            self.target_policy = self._make_target_policy()
