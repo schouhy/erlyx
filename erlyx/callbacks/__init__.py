9db7b3509dac0e80902123c4393273b9507a13ad
Sergio Chouhy <sergio.chouhy@gmail.com>
2020-08-17 14:40:01 -0300

    removed some imports from __init__.py

--- erlyx/callbacks/__init__.py
+++ erlyx/callbacks/__init__.py
@@ -1,5 +1,6 @@
 from erlyx.callbacks.base import BaseCallback, CallbackTuple
-from erlyx.callbacks.recorders import (Transition, TransitionRecorder, TransitionSequenceRecorder, RewardRecorder,
-                                       AgentVersionRecorder)
-from erlyx.callbacks.updaters import OnlineUpdater, ExponentialEpsilonDecay, LinearEpsilonDecay
-from erlyx.callbacks.renderers import GymRenderer
+#from erlyx.callbacks.recorders import (Transition, TransitionRecorder, TransitionSequenceRecorder, RewardRecorder,
+#                                      AgentVersionRecorder)
+#from erlyx.callbacks.updaters import OnlineUpdater, ExponentialEpsilonDecay, LinearEpsilonDecay
+#from erlyx.callbacks.renderers import GymRenderer
+#from erlyx.callbacks.checkpoint import PytorchCheckPointer
