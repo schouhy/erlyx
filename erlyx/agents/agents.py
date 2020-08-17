cb2f9591b652fa377834c485ff65feacaa1fcf18
Sergio Chouhy <sergio.chouhy@gmail.com>
2020-08-17 14:40:31 -0300

    added epsilon greedy agent that supports states history stacking

--- erlyx/agents/agents.py
+++ erlyx/agents/agents.py
@@ -4,6 +4,8 @@ from erlyx import types
 
 import numpy as np
 
+from collections import deque
+
 
 class EpsilonGreedyAgent(BaseAgent):
     def __init__(self, policy: Policy, epsilon):
@@ -13,6 +15,25 @@ class EpsilonGreedyAgent(BaseAgent):
     def select_action(self, observation: types.ObservationType) -> types.ActionType:
         if np.random.uniform() < self.epsilon:
             return np.random.choice(self.action_space)
+        distribution = self.policy.get_distribution(observation)
+        return np.argmax(distribution)
+
+
+class EpsilonGreedyHistoryAgent(BaseAgent):
+    def __init__(self, policy: Policy, epsilon, obs_shape):
+        super(EpsilonGreedyHistoryAgent, self).__init__(policy=policy)
+        self.epsilon = epsilon
+        self._obs_shape = obs_shape
+        self._memory_buffer = deque(maxlen=4)
+
+    def reset_memory(self):
+        self._memory_buffer = deque([np.zeros(shape=self._obs_shape)] * 4, maxlen=4)
+
+    def select_action(self, observation: types.ObservationType) -> types.ActionType:
+        self._memory_buffer.append(observation)
+        if np.random.uniform() < self.epsilon:
+            return np.random.choice(self.action_space)
+        observation = np.asarray(list(self._memory_buffer))
         distribution = self.policy.get_distribution(observation)
         return np.argmax(distribution)
 
